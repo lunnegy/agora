41b49489e8ace92e1538a30522b8426008ef5b90
Glenn Yu <glennyu@stanford.edu>
2021-04-04 14:10:25 -0400

    got pic and text side by side

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and be the most awesome.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>agora</h1>
+        <h5>a new way to connect</h5>
+      </header> 
+
+      <div className="Profile">
+        <img src={logo}/>
+        <div>
+          <b>Glenn Yu\adlfjlksd</b>
+        </div>
+        <div>
+          I like ot eat
+        </div>
+      </div>
     </div>
   );
 }
 
+
+// ===================================
+
 export default App;
+
