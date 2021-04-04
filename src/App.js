032a1e612b57801a6bf2d93d8288106ea2326945
Glenn Yu <glennyu@stanford.edu>
2021-04-04 00:58:35 -0400

    test App.js again

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and be awesome.
+          Edit <code>src/App.js</code> and be the most awesome.
         </p>
         <a
           className="App-link"
